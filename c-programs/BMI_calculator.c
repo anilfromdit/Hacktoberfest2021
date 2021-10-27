56a116374c9cb132f639f0a865197020617eb80c
Anil Gulati <72304697+anilfromdit@users.noreply.github.com>
2021-10-27 14:57:47 +0530

    Update BMI_calculator.c

--- c-programs/BMI_calculator.c
+++ c-programs/BMI_calculator.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
 int main() {
+	while(true){
 	  double x,y;
 	      printf("Enter your height in meter:");
 		    scanf("%lf",&x);
 		        printf("Enter your weight in KiloGrams (kg):");
 			    scanf("%lf",&y);
 			        double bmi=y/(x*x);
-				    printf("your BMI is : %lf", bmi);
-
+				    printf("Your BMI is : %lf", bmi);
+		printf("Do you want to restart the program?(Y/N)");
+		char c;
+		scanf("%c",&c);
+		if(c!='Y'){
+		return 0;
+		}
+	}
 				        return 0;
 }
 
